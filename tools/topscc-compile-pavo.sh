7df6125684d79a1dadf209993534ba2395410c5d
Albert.Shi <heng.shi@enflame-tech.com>
2023-09-15 07:36:31 +0000

    add dorado kernels; add basic kernel compiling script

---
+++ tools/topscc-compile-pavo.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+SCRIPT_PATH=`dirname $0`
+KERNEL_PATH=`realpath $SCRIPT_PATH"/../kernels/pavo/"`
+
+if [ $# -gt 1 ]; then
+    echo "please specify kernel-name to be built"
+else
+    topscc $KERNEL_PATH"/../"$1.cpp -arch gcu210 -ltops -std=c++17 -lpthread -O0 -o $KERNEL_PATH"/"$1.o0.out --save-temps
+fi
+
+# cleaning up tmp files
+mv $1*.topsfb $KERNEL_PATH/$1.topsfb
+rm $1-tops-dtu-enflame-tops-gcu210.*
+rm $1-host-x86_64-unknown-linux-gnu.*
+rm $1-tops-dtu-enflame-tops-gcu210*
+rm a.out*
